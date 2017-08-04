f05109cd8dd4096c38e2c4f093bd7bb0b8314bd2
swarooppallapothu <swaroop.pallapothu@gmail.com>
2017-08-04 16:13:39 +0530

    init commit

---
+++ src/main/java/com/jsonpoc/Client.java
@@ -0,0 +1,18 @@
+package com.jsonpoc;
+
+import com.jsonpoc.component.ValidateComponent;
+import com.jsonpoc.dto.ValidationStatus;
+
+public class Client {
+
+    private static String dataFilePath = System.getProperty("user.dir") + "/src/main/resources/data.json";
+    private static String schemaFilePath = System.getProperty("user.dir") + "/src/main/resources/schema.json";
+    private static String validationRulesFilePath = System.getProperty("user.dir") + "/src/main/resources/validations.json";
+
+    public static void main(String[] args) {
+        ValidateComponent validateComponent = new ValidateComponent();
+        //Validating schema
+        ValidationStatus validationScheamStatus = validateComponent.validateDataWithSchema(schemaFilePath, dataFilePath);
+        System.out.println(validationScheamStatus);
+    }
+}
